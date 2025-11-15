d1a46bde14ebef7bbd5f7bab6684dc2ea322c232
frank <wz2cool@live.cn>
2025-11-15 22:29:46 +0800

    done

--- src/main/java/com/github/wz2cool/mybatis/dynamic/query/sqlitedemo/mapper/CategoryMapper.java
+++ src/main/java/com/github/wz2cool/mybatis/dynamic/query/sqlitedemo/mapper/CategoryMapper.java
@@ -1,13 +1,11 @@
 package com.github.wz2cool.mybatis.dynamic.query.sqlitedemo.mapper;
 
-import com.github.wz2cool.mybatis.dynamic.query.sqlitedemo.model.entity.CategoryDO;
 import com.github.wz2cool.dynamic.mybatis.mapper.DynamicQueryMapper;
-import org.apache.ibatis.annotations.Mapper;
+import com.github.wz2cool.mybatis.dynamic.query.sqlitedemo.model.entity.CategoryDO;
 
 /**
  * 分类表Mapper接口
  */
-@Mapper
 public interface CategoryMapper extends DynamicQueryMapper<CategoryDO> {
     // 继承DynamicQueryMapper后，自动拥有基础的CRUD和动态查询方法
 }
--- src/main/java/com/github/wz2cool/mybatis/dynamic/query/sqlitedemo/mapper/ProductMapper.java
+++ src/main/java/com/github/wz2cool/mybatis/dynamic/query/sqlitedemo/mapper/ProductMapper.java
@@ -7,7 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 /**
  * 产品表Mapper接口
  */
-@Mapper
 public interface ProductMapper extends DynamicQueryMapper<ProductDO> {
     // 继承DynamicQueryMapper后，自动拥有基础的CRUD和动态查询方法
 }
